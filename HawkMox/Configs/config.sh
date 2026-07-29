eb37d9d7c7dd5215a2a0ef6114e11f981b837507
Partybringer13 <97998886+Partybringer13@users.noreply.github.com>
2026-07-28 21:59:50 -0500

    Add configuration script for HawkMox
    This script contains configuration settings for HawkMox, including storage, ZFS options, directory paths, cluster name, logging, and color codes.

---
+++ HawkMox/Configs/config.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+############################################################
+# HawkMox Configuration
+############################################################
+
+#
+# Storage
+#
+
+BOOT_DISK="/dev/mmcblk0"
+APP_DISK="/dev/sda"
+
+POOL_NAME="hawktank"
+
+VM_DATASET="vmstore"
+
+FILE_DATASET="hawkfiles"
+
+FILE_MOUNT="/srv/hawkmox"
+
+#
+# Proxmox Storage IDs
+#
+
+ZFS_STORAGE_ID="hawktank"
+
+DIR_STORAGE_ID="hawkfiles"
+
+#
+# ZFS
+#
+
+ASHIFT="12"
+
+COMPRESSION="lz4"
+
+ATIME="off"
+
+AUTOTRIM="on"
+
+#
+# Directories
+#
+
+ISO_DIR="${FILE_MOUNT}/iso"
+
+TEMPLATE_DIR="${FILE_MOUNT}/templates"
+
+BACKUP_DIR="${FILE_MOUNT}/backups"
+
+SNIPPET_DIR="${FILE_MOUNT}/snippets"
+
+IMPORT_DIR="${FILE_MOUNT}/import"
+
+#
+# Cluster
+#
+
+CLUSTER_NAME="HawkCluster"
+
+#
+# Logging
+#
+
+LOG_FILE="/var/log/hawkmox.log"
+
+#
+# Colors
+#
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+NC="\033[0m"
