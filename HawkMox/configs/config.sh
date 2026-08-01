1ff2b9ed012a633ebc35bb8dd18a85d0c6b795c3
Partybringer13 <97998886+Partybringer13@users.noreply.github.com>
2026-08-01 00:59:12 -0500

    Update configuration values and formatting in config.sh

--- HawkMox/configs/config.sh
+++ HawkMox/configs/config.sh
@@ -4,32 +4,33 @@
 # HawkMox Configuration
 ############################################################
 
-#
+############################
 # Storage
-#
+############################
 
 BOOT_DISK="/dev/mmcblk0"
+
 APP_DISK="/dev/sda"
 
 POOL_NAME="hawktank"
 
 VM_DATASET="vmstore"
 
-FILE_DATASET="hawkfiles"
+FILE_DATASET="files"
 
 FILE_MOUNT="/srv/hawkmox"
 
-#
+############################
 # Proxmox Storage IDs
-#
+############################
 
 ZFS_STORAGE_ID="hawktank"
 
 DIR_STORAGE_ID="hawkfiles"
 
-#
-# ZFS
-#
+############################
+# ZFS Defaults
+############################
 
 ASHIFT="12"
 
@@ -39,9 +40,9 @@ ATIME="off"
 
 AUTOTRIM="on"
 
-#
+############################
 # Directories
-#
+############################
 
 ISO_DIR="${FILE_MOUNT}/iso"
 
@@ -53,25 +54,26 @@ SNIPPET_DIR="${FILE_MOUNT}/snippets"
 
 IMPORT_DIR="${FILE_MOUNT}/import"
 
-#
+############################
 # Cluster
-#
+############################
 
 CLUSTER_NAME="HawkCluster"
+
 CLUSTER_MASTER_IP="192.168.1.10"
 
-#
+############################
 # Logging
-#
+############################
 
 LOG_FILE="/var/log/hawkmox.log"
 
-#
+############################
 # Colors
-#
+############################
 
-RED="\033[1;31m"
-GREEN="\033[1;32m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
 YELLOW="\033[1;33m"
-BLUE="\033[1;34m"
+BLUE="\033[0;34m"
 NC="\033[0m"
